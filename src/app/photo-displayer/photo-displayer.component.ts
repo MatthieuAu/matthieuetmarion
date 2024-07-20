af7e57321bf246f400de6e61a9af9b6de3313264
Matthieu Aubry <matthieu.aubry@neoxia.com>
2024-07-20 16:20:40 +0200

    bug fixes

--- src/app/photo-displayer/photo-displayer.component.ts
+++ src/app/photo-displayer/photo-displayer.component.ts
@@ -11,45 +11,61 @@ export class PhotoDisplayerComponent implements OnInit{
   parser = new DOMParser();
   lastPhotoName: string = "";
   needToFetchNextPhotos = true;
-  maxPhotosPerFetch = 30;
+  maxPhotosPerFetch = 15;
   modalImage="";
   showPhotos = false;
   mdpSubmission = "";
+  fetching = false;
 
   constructor(private http: HttpClient) {
   }
   
   
   ngOnInit(): void {
-    this.http.get(`https://mariage-civil.s3.eu-west-3.amazonaws.com/?list-type=2&max-keys=${this.maxPhotosPerFetch}`, { responseType: 'text' }).subscribe(
-      (value)=> {
-        const parsed = this.parser.parseFromString(value, "text/xml");
-        const array = Array.from(parsed.querySelectorAll("Key"));
-        this.photosName = array.map((value) => "https://mariage-civil.s3.eu-west-3.amazonaws.com/" + value.innerHTML);
-        this.lastPhotoName = array.at(-1)!.innerHTML;
-        if (array.length == 0 || array.length < this.maxPhotosPerFetch) {
-          this.needToFetchNextPhotos = false;
-        } 
-      }
-    );
-  }
-
-  onScrollended() {
-    if (this.needToFetchNextPhotos) {
-      console.log("Fetching...")
-      this.http.get(`https://mariage-civil.s3.eu-west-3.amazonaws.com/?list-type=2&max-keys=${this.maxPhotosPerFetch}&start-after=${this.lastPhotoName}`, { responseType: 'text' }).subscribe(
+    this.fetching = true;
+    try {
+      this.http.get(`https://mariage-civil.s3.eu-west-3.amazonaws.com/?list-type=2&max-keys=${this.maxPhotosPerFetch}`, { responseType: 'text' }).subscribe(
         (value)=> {
-          console.log("Parsing...")
           const parsed = this.parser.parseFromString(value, "text/xml");
           const array = Array.from(parsed.querySelectorAll("Key"));
+          this.photosName = array.map((value) => "https://mariage-civil.s3.eu-west-3.amazonaws.com/" + value.innerHTML);
+          this.lastPhotoName = array.at(-1)!.innerHTML;
           if (array.length == 0 || array.length < this.maxPhotosPerFetch) {
             this.needToFetchNextPhotos = false;
           } 
-          if (array.length > 0) {
-            this.photosName.push(...array.map((value) => "https://mariage-civil.s3.eu-west-3.amazonaws.com/" + value.innerHTML));
-            this.lastPhotoName = array.at(-1)!.innerHTML;
-          }
-      });
+          this.fetching = false;
+        }
+      );
+    } catch (e) {
+      console.error(e)
+      this.fetching = false;
+    }
+    
+  }
+
+  onScrollended() {
+    if (this.needToFetchNextPhotos && !this.fetching) {
+      console.log("Fetching...")
+      this.fetching = true;
+      try {
+        this.http.get(`https://mariage-civil.s3.eu-west-3.amazonaws.com/?list-type=2&max-keys=${this.maxPhotosPerFetch}&start-after=${this.lastPhotoName}`, { responseType: 'text' }).subscribe(
+          (value)=> {
+            console.log("Parsing...")
+            const parsed = this.parser.parseFromString(value, "text/xml");
+            const array = Array.from(parsed.querySelectorAll("Key"));
+            if (array.length == 0 || array.length < this.maxPhotosPerFetch) {
+              this.needToFetchNextPhotos = false;
+            } 
+            if (array.length > 0) {
+              this.photosName.push(...array.map((value) => "https://mariage-civil.s3.eu-west-3.amazonaws.com/" + value.innerHTML));
+              this.lastPhotoName = array.at(-1)!.innerHTML;
+            }
+            this.fetching = false
+        });
+      } catch (e) {
+        console.error(e)
+        this.fetching = false;
+      }
     }
   }
 
